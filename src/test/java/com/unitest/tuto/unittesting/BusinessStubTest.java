ebf0cc55ab56de5132049b2eab3e3352d409ae61
quentin.bolsee <quentin.bolsee@lampiris.be>
2022-05-13 10:45:47 +0200

    Writing your first unit test with Stub

--- src/test/java/com/unitest/tuto/unittesting/BusinessStubTest.java
+++ src/test/java/com/unitest/tuto/unittesting/BusinessStubTest.java
@@ -2,21 +2,32 @@ package com.unitest.tuto.unittesting;
 
 
 import com.unitest.tuto.unittesting.business.BusinessImpl;
+import com.unitest.tuto.unittesting.data.SomeDataService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Service
+class SomeDataServiceStub implements SomeDataService {
+
+    @Override
+    public int[] retrieveAllData() {
+        return new int[] {1,2,3};
+    }
+}
+
 @SpringBootTest
-public class BusinessTest {
+public class BusinessStubTest {
 
     @Autowired
     private BusinessImpl business;
 
     @Test
     public void calculateSum_basic() {
-        int actualResult = business.calculateSum(new int [] {1,2,3});
+        int actualResult = business.calculateSumUsingDataService();
         int expectedResult = 6;
 
         assertEquals(expectedResult, actualResult);
@@ -24,7 +35,7 @@ public class BusinessTest {
 
     @Test
     public void calculateSum_emptyArray() {
-        int actualResult = business.calculateSum(new int [] {});
+        int actualResult = business.calculateSumUsingDataService();
         int expectedResult = 0;
 
         assertEquals(expectedResult, actualResult);
@@ -32,7 +43,7 @@ public class BusinessTest {
 
     @Test
     public void calculateSum_oneValue() {
-        int actualResult = business.calculateSum(new int [] {5});
+        int actualResult = business.calculateSumUsingDataService();
         int expectedResult = 5;
 
         assertEquals(expectedResult, actualResult);
