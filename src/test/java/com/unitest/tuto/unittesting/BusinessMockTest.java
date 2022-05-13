7ede1f8105e077c60f27dda8215ae322f74d2fb7
secretMoi <win.moi@hotmail.com>
2022-05-13 11:01:42 +0200

    More Refactoring - @Mock, @InjectMocks and @RunWith(MockitoJUnitRunner

--- src/test/java/com/unitest/tuto/unittesting/BusinessMockTest.java
+++ src/test/java/com/unitest/tuto/unittesting/BusinessMockTest.java
@@ -5,28 +5,26 @@ import com.unitest.tuto.unittesting.business.BusinessImpl;
 import com.unitest.tuto.unittesting.data.SomeDataService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
-public class BusinessMockTest {
+class BusinessMockTest {
 
+    @InjectMocks
     private BusinessImpl business;
 
-    @BeforeEach
-    private void setTestEnvironment() {
-        business = new BusinessImpl();
-    }
+    @Mock
+    private SomeDataService dataServiceMock = mock(SomeDataService.class); // création du mock
 
     @Test
-    public void calculateSum_basic() {
+    void calculateSum_basic() {
 
-        var dataServiceMock = mock(SomeDataService.class); // création du mock
         when(dataServiceMock.retrieveAllData()).thenReturn(new int [] {1,2,3}); // mock les données
         business.setSomeDataService(dataServiceMock);
 
@@ -37,9 +35,8 @@ public class BusinessMockTest {
     }
 
     @Test
-    public void calculateSum_emptyArray() {
+    void calculateSum_emptyArray() {
 
-        var dataServiceMock = mock(SomeDataService.class); // création du mock
         when(dataServiceMock.retrieveAllData()).thenReturn(new int [] {}); // mock les données
         business.setSomeDataService(dataServiceMock);
 
@@ -50,8 +47,8 @@ public class BusinessMockTest {
     }
 
     @Test
-    public void calculateSum_oneValue() {
-        var dataServiceMock = mock(SomeDataService.class); // création du mock
+    void calculateSum_oneValue() {
+
         when(dataServiceMock.retrieveAllData()).thenReturn(new int [] {5}); // mock les données
         business.setSomeDataService(dataServiceMock);
 
