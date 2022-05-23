bd6d3ffb26b650373d1586506f7b2be5b606592a
quentin.bolsee <quentin.bolsee@lampiris.be>
2022-05-23 14:38:47 +0200

    Step 01 - Creating a Hello World Controller

---
+++ src/main/java/com/unitest/tuto/unittesting/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.unitest.tuto.unittesting.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
