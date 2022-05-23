8bbeae3f09775a68be509b60d6f42d269b00b1bd
quentin.bolsee <quentin.bolsee@lampiris.be>
2022-05-23 16:45:02 +0200

    Step 07 - Writing a REST Service talking to Business Layer

---
+++ src/main/java/com/unitest/tuto/unittesting/business/ItemBusinessService.java
@@ -0,0 +1,12 @@
+package com.unitest.tuto.unittesting.business;
+
+import com.unitest.tuto.unittesting.data.Item;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemBusinessService {
+
+    public Item retrieveHardCodedItem() {
+        return new Item(1, "Ball", 10, 100);
+    }
+}
--- src/main/java/com/unitest/tuto/unittesting/controller/ItemController.java
+++ src/main/java/com/unitest/tuto/unittesting/controller/ItemController.java
@@ -1,14 +1,24 @@
 package com.unitest.tuto.unittesting.controller;
 
+import com.unitest.tuto.unittesting.business.ItemBusinessService;
 import com.unitest.tuto.unittesting.data.Item;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ItemController {
 
+    @Autowired
+    private ItemBusinessService itemBusinessService;
+
     @GetMapping("/dummy-item")
     public Item dummyItem() {
         return new Item(1, "Ball", 10, 100);
+    }
+
+    @GetMapping("/item-from-business-service")
+    public Item itemFromBusinessService() {
+        return itemBusinessService.retrieveHardCodedItem();
     }
 }
