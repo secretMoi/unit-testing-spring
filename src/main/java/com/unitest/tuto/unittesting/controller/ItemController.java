7e629cc2a6989ace81fefefe4a519b87eaa76b70
secretMoi <win.moi@hotmail.com>
2022-05-23 15:59:20 +0200

    Step 04 - Creating a Basic REST Service in Item Controller

---
+++ src/main/java/com/unitest/tuto/unittesting/controller/ItemController.java
@@ -0,0 +1,14 @@
+package com.unitest.tuto.unittesting.controller;
+
+import com.unitest.tuto.unittesting.data.Item;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemController {
+
+    @GetMapping("/dummy-item")
+    public Item dummyItem() {
+        return new Item(1, "Ball", 10, 100);
+    }
+}
