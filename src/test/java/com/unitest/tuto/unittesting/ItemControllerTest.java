bc9d4b57e21779b24db11e7b62c9bad4b7f87483
quentin.bolsee <quentin.bolsee@lampiris.be>
2022-05-23 16:01:56 +0200

    Step 05 - Unit Testing Item Controller and Basic JSON Assertions

---
+++ src/test/java/com/unitest/tuto/unittesting/ItemControllerTest.java
@@ -0,0 +1,37 @@
+package com.unitest.tuto.unittesting;
+
+import com.unitest.tuto.unittesting.controller.ItemController;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ItemController.class)
+class ItemControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void dummyItem_basic() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/dummy-item")
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc
+                .perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"))
+                .andReturn();
+
+        Assertions.assertEquals("Hello World", result.getResponse().getContentAsString());
+    }
+}
