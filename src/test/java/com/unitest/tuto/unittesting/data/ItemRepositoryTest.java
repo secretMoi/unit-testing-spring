eac5cbcc3dfe8dce0147cea9872b9ba4f823efa2
quentin.bolsee <quentin.bolsee@lampiris.be>
2022-05-27 11:14:59 +0200

    Step 14 - Writing Unit Test for Data Layer - Data JPA Test

---
+++ src/test/java/com/unitest/tuto/unittesting/data/ItemRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.unitest.tuto.unittesting.data;
+
+import com.unitest.tuto.unittesting.repository.ItemRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+class ItemRepositoryTest {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Test
+    void testFindAll() {
+        var items = itemRepository.findAll();
+
+        assertEquals(3 , items.size());
+    }
+}
