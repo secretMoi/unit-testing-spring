9bd09e9787cd0e14adfeee5604832dad4c4e3fab
secretMoi <win.moi@hotmail.com>
2022-05-27 11:00:47 +0200

    Step 12 - Writing Unit Test for Web Layer - Controller - Using Mock MVC

--- src/test/java/com/unitest/tuto/unittesting/controller/ItemControllerTest.java
+++ src/test/java/com/unitest/tuto/unittesting/controller/ItemControllerTest.java
@@ -14,6 +14,10 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -45,7 +49,7 @@ class ItemControllerTest {
     @Test
     void itemFromBusinessService_basic() throws Exception {
         when(itemBusinessService.retrieveHardCodedItem()).thenReturn(
-                new Item(2, "Item", 10, 10)
+                new Item(2, "Item", 10, 10, 0)
         );
 
         RequestBuilder requestBuilder = MockMvcRequestBuilders
@@ -59,5 +63,25 @@ class ItemControllerTest {
                 .andReturn();
 
 //        Assertions.assertEquals("Hello World", result.getResponse().getContentAsString());
+    }
+
+    @Test
+    void retrieveAllItems_basic() throws Exception {
+        when(itemBusinessService.retrieveAllItems()).thenReturn(
+                List.of(
+                        new Item(2, "Item", 10, 10, 0),
+                        new Item(3, "Item3", 20, 20, 0)
+                )
+        );
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/all-items-from-database")
+                .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc
+                .perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{id:2,name:Item,price:10,quantity:10},{id:3,name:Item3,price:20,quantity:20}]"))
+                .andReturn();
     }
 }
