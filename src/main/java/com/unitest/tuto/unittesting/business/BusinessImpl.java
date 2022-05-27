18462c2ce4e18e8721630469920c2175243012d2
quentin.bolsee <quentin.bolsee@lampiris.be>
2022-05-27 11:31:40 +0200

    Step 19 - Refactor SomeBusinessImpl to use Functional Programming

--- src/main/java/com/unitest/tuto/unittesting/business/BusinessImpl.java
+++ src/main/java/com/unitest/tuto/unittesting/business/BusinessImpl.java
@@ -4,6 +4,8 @@ import com.unitest.tuto.unittesting.data.SomeDataService;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
 @Setter
 public class BusinessImpl {
@@ -11,24 +13,14 @@ public class BusinessImpl {
     private SomeDataService someDataService;
 
     public int calculateSum(int[] data) {
-        int sum = 0;
-
-        for(int value : data) {
-            sum += value;
-        }
-
-        return sum;
+        // 1, 2, 3, 4 => 1 + 2 + 3 + 4
+        return Arrays.stream(data).reduce(Integer::sum).orElse(0);
     }
 
     public int calculateSumUsingDataService() {
-        int sum = 0;
         int[] data = someDataService.retrieveAllData();
 
-        for(int value : data) {
-            sum += value;
-        }
-
-//        someDataService.storeSum(sum);
-        return sum;
+        // 1, 2, 3, 4 => 1 + 2 + 3 + 4
+        return Arrays.stream(data).reduce(Integer::sum).orElse(0);
     }
 }
